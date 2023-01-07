25c91cd9362a8f3a44ca20133f19120911d176d1
FathinAhmed2003 <117044979+FathinAhmed2003@users.noreply.github.com>
2023-01-06 23:12:40 -0500

    Add files via upload
    Guessing game to guess a secret word. You only have three guesses.

---
+++ guessing_game.py
@@ -0,0 +1,25 @@
+secret_word = "hoe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("What is the secret word? \n")
+        guess_count += 1
+        if guess != secret_word:
+            print("Please try again.\n")
+
+    else:
+        out_of_guesses = True
+
+if guess == secret_word:
+    print("YOU WIN!!")
+else:
+    print("Your out of guesses!")
+
+
+
+
+
